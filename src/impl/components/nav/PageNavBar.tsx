817d3788a0bcb94819988482374decde40b6dc0e
sverg84 <134344913+sverg84@users.noreply.github.com>
2023-08-14 10:10:49 -0700

    Testing React Router

--- src/impl/components/nav/PageNavBar.tsx
+++ src/impl/components/nav/PageNavBar.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 import MyName from '../../consts/MyName.ts';
 import useBackgroundColorStyle from '../../hooks/useBackgroundColorStyle.ts';
+import brandStyles from '../../styles/nav/brand.module.css';
 import pageStyles from '../../styles/pageMargin.module.css';
 import PageNavColorPicker from './PageNavColorPicker.tsx';
 
@@ -17,7 +19,15 @@ export default function PageNavBar(): React.JSX.Element {
 			sticky="top"
 			variant="dark">
 			<Container className={pageStyles.page}>
-				<Navbar.Brand>{MyName}</Navbar.Brand>
+				<Navbar.Brand>
+					{
+						<Link
+							className={brandStyles.link}
+							to="/2nd">
+							{MyName}
+						</Link>
+					}
+				</Navbar.Brand>
 				<PageNavColorPicker />
 			</Container>
 		</Navbar>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,29 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import * as React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import App from "./impl/components/App";
+import App from './impl/components/App';
 
-const rootElement: HTMLElement | null = document.getElementById("root");
+const rootElement: HTMLElement | null = document.getElementById('root');
 
 if (rootElement == null) {
-  throw new Error("Failed to find root element");
+	throw new Error('Failed to find root element');
 }
 
 const root: ReactDOM.Root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					element={<App />}
+					path="/"
+				/>
+        <Route element={<h2>Test</h2>} path="/2nd" />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>,
 );
