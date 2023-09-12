7f862634a6d3dce1728b666d83b90803fd81deb8
sverg84 <134344913+sverg84@users.noreply.github.com>
2023-09-12 15:17:21 -0700

    change star color based on context value

--- src/impl/components/body/personal/PersonalBackground.tsx
+++ src/impl/components/body/personal/PersonalBackground.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react';
 
+import PageColorContext from '../../../contexts/PageColorContext';
 import styles from '../../../styles/body/personal/background.module.scss';
+import { PAGE_COLORS } from '../../../types/PageColor';
+
+function useStyle(): string {
+	const color = React.useContext(PageColorContext);
+
+	switch (color) {
+		case PAGE_COLORS.Obsidian:
+			return styles.obsidian;
+		case PAGE_COLORS['Baby pink']:
+			return styles.babypink;
+		case PAGE_COLORS['Royal blue']:
+			return styles.royalblue;
+		case PAGE_COLORS['Phoenix scarlet']:
+			return styles.phoenixscarlet;
+		case PAGE_COLORS['Grape jelly']:
+			return styles.grapejelly;
+		case PAGE_COLORS['Ogre green']:
+			return styles.ogregreen;
+	}
+}
 
 export default function PersonalBackground(): React.JSX.Element {
+	const style = useStyle();
+
 	return (
-		<div className={styles.bg}>
+		<div className={style}>
 			{[styles.stars, styles.stars2, styles.stars3].map(style => (
 				<div
 					className={style}
