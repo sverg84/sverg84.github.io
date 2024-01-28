e0066c531d5dfe4a67fef60e0c649cf6e52e88bf
sverg84 <134344913+sverg84@users.noreply.github.com>
2024-01-28 00:29:47 -0800

    1st fetch: add relative time since last update to page

--- src/impl/components/nav/PageNavBar.tsx
+++ src/impl/components/nav/PageNavBar.tsx
@@ -3,13 +3,14 @@ import * as React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/esm/Col';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import MyName from '../../consts/MyName.ts';
 import useBackgroundColorStyle from '../../hooks/useBackgroundColorStyle.ts';
 import navStyles from '../../styles/nav/navbar.module.scss';
 import pageStyles from '../../styles/pageMargin.module.scss';
 import PageNavColorPicker from './PageNavColorPicker.tsx';
+import PageNavLatestPushTimestamp from './PageNavLatestPushTimestamp.tsx';
 
 type Props = Readonly<{
 	breadcrumbs?: React.JSX.Element | undefined;
@@ -17,6 +18,7 @@ type Props = Readonly<{
 
 export default function PageNavBar({breadcrumbs}: Props): React.JSX.Element {
 	const style = useBackgroundColorStyle();
+
 	return (
 		<Navbar
 			expand={false}
@@ -29,6 +31,9 @@ export default function PageNavBar({breadcrumbs}: Props): React.JSX.Element {
 						to="/">
 						{MyName}
 					</Link>
+				</Col>
+				<Col className={navStyles.pushTime}>
+					<PageNavLatestPushTimestamp />
 				</Col>
 				<Col className={navStyles.rightContent}>
 					{breadcrumbs}
---
+++ src/impl/components/nav/PageNavLatestPushTimestamp.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+
+import AppTooltip from '../AppTooltip';
+
+// https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time
+const RTF = new Intl.RelativeTimeFormat(undefined, {numeric: 'auto'});
+
+const UNITS: {unit: Intl.RelativeTimeFormatUnit; ms: number}[] = [
+	{unit: 'year', ms: 31536000000},
+	{unit: 'month', ms: 2628000000},
+	{unit: 'day', ms: 86400000},
+	{unit: 'hour', ms: 3600000},
+	{unit: 'minute', ms: 60000},
+	{unit: 'second', ms: 1000},
+];
+
+function relativeTimeFromElapsed(elapsed: number): string {
+	for (const {unit, ms} of UNITS) {
+		if (Math.abs(elapsed) >= ms || unit === 'second') {
+			return RTF.format(Math.round(elapsed / ms), unit);
+		}
+	}
+	return '';
+}
+
+export default function PageNavLatestPushTimestamp(): React.JSX.Element | null {
+	const [lastUpdatedTime, setLastUpdatedTime] = React.useState<Date | null>(
+		null,
+	);
+
+	React.useEffect(() => {
+		const fetchTime = async () => {
+			try {
+				const response = await fetch(
+					'https://api.github.com/repos/sverg84/sverg84.github.io',
+				);
+				if (!response.ok) {
+					throw new Error(`${response.status}: ${response.statusText}`);
+				}
+				const data = await response.json();
+				const updateTime: string = data['pushed_at'];
+				setLastUpdatedTime(new Date(updateTime));
+			} catch (error) {
+				console.warn(error);
+			}
+		};
+
+		fetchTime();
+	}, []);
+
+	if (lastUpdatedTime == null) {
+		return null;
+	}
+
+	const timeSinceLastUpdate = lastUpdatedTime.getTime() - new Date().getTime();
+
+	return (
+		<>
+			Last Updated:{' '}
+			<AppTooltip
+				content={relativeTimeFromElapsed(timeSinceLastUpdate)}
+				tooltip={lastUpdatedTime.toLocaleString(undefined)}
+			/>
+		</>
+	);
+}
